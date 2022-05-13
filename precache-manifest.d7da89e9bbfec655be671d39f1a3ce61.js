3f3da399d83769c96f505b61c95a7258959d9814
nishes gurung <nishesgrg16@gmail.com>
2022-05-13 15:42:38 +0545

    Updates

---
+++ precache-manifest.d7da89e9bbfec655be671d39f1a3ce61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "041aaf0008cae9d2a796",
+    "url": "/Reactproject.medical/static/css/main.e0e842aa.chunk.css"
+  },
+  {
+    "revision": "041aaf0008cae9d2a796",
+    "url": "/Reactproject.medical/static/js/main.041aaf00.chunk.js"
+  },
+  {
+    "revision": "88cdf4e90e711c3ae1e4",
+    "url": "/Reactproject.medical/static/js/1.88cdf4e9.chunk.js"
+  },
+  {
+    "revision": "360cda7f7178d340695f",
+    "url": "/Reactproject.medical/static/js/2.360cda7f.chunk.js"
+  },
+  {
+    "revision": "6186e455075360d9f367",
+    "url": "/Reactproject.medical/static/js/runtime~main.6186e455.js"
+  },
+  {
+    "revision": "ca4adbd991378254b0dca2cb956fc5b0",
+    "url": "/Reactproject.medical/static/media/about.ca4adbd9.jpg"
+  },
+  {
+    "revision": "1cd76efcd7311edfe6654a07bb8689cc",
+    "url": "/Reactproject.medical/static/media/hero-bg.1cd76efc.jpg"
+  },
+  {
+    "revision": "67d614c290cf8a4c59d442b4af703763",
+    "url": "/Reactproject.medical/index.html"
+  }
+];
